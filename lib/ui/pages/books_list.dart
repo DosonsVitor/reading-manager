61abe6802ffe2dbc5dcd1f2c0536f6a3babf3956
DosonsVitor <dosons123@gmail.com>
2023-08-05 11:32:56 -0300

    criar tela de lista de livros

---
+++ lib/ui/pages/books_list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class BooksList extends StatefulWidget{
+  const BooksList({super.key});
+
+  @override
+  State<BooksList> createState() => _BooksListState();
+}
+
+class _BooksListState extends State<BooksList>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
