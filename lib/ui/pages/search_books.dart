4b86a518c7938ecc796f0851116513d4ee9631c9
DosonsVitor <dosons123@gmail.com>
2023-08-05 11:35:50 -0300

    criar tela de buscar livros

---
+++ lib/ui/pages/search_books.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class SearchBooks extends StatefulWidget{
+  const SearchBooks({super.key});
+
+  @override
+  State<SearchBooks> createState() => _SearchBooksState();
+}
+
+class _SearchBooksState extends State<SearchBooks>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
