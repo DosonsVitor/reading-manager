274e4e5e9d9e5a5b29782a92a7a9971d567e4d65
DosonsVitor <dosons123@gmail.com>
2023-08-05 11:34:18 -0300

    criar tela de detalhes do livro

---
+++ lib/ui/pages/book_details.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class BookDetails extends StatefulWidget{
+  const BookDetails({super.key});
+
+  @override
+  State<BookDetails> createState() => _BookDetailsState();
+}
+
+class _BookDetailsState extends State<BookDetails>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
