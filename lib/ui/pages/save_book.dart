3a0c7d20d5523c3eaf3b462eca2e2c7eba023ad2
DosonsVitor <dosons123@gmail.com>
2023-08-05 11:38:03 -0300

    criar tela de salvar e adicionar livro

---
+++ lib/ui/pages/save_book.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class SaveBook extends StatefulWidget{
+  const SaveBook({super.key});
+
+  @override
+  State<SaveBook> createState() => _SaveBookState();
+}
+
+class _SaveBookState extends State<SaveBook>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
