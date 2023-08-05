000528812841877d9e4b70606f5223ce52f5ea77
DosonsVitor <dosons123@gmail.com>
2023-08-05 14:33:57 -0300

    configurar tema do aplicativo

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:reading_manager/ui/pages/initial_page.dart';
+import 'package:reading_manager/ui/style.dart';
 
 class App extends StatelessWidget{
   const App({super.key});
@@ -7,12 +8,13 @@ class App extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: themeLight,
+      darkTheme: themeDark,
+      themeMode: ThemeMode.system,
       initialRoute: InitialPage.route,
       routes: {
         InitialPage.route: (context) => const InitialPage(),
       },
     );
   }
-
-
 }
---
+++ lib/ui/style.dart
@@ -0,0 +1,29 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
+
+ThemeData themeLight = FlexThemeData.light(
+  scheme: FlexScheme.blueWhale,
+  surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+  blendLevel: 7,
+  subThemesData: const FlexSubThemesData(
+    blendOnLevel: 10,
+    blendOnColors: false,
+    useTextTheme: false,
+    useM2StyleDividerInM3: true,
+  ),
+  visualDensity: FlexColorScheme.comfortablePlatformDensity,
+  useMaterial3: true,
+);
+
+ThemeData themeDark = FlexThemeData.dark(
+  scheme: FlexScheme.blueWhale,
+  surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+  blendLevel: 13,
+  subThemesData: const FlexSubThemesData(
+    blendOnLevel: 20,
+    useTextTheme: false,
+    useM2StyleDividerInM3: true,
+  ),
+  visualDensity: FlexColorScheme.comfortablePlatformDensity,
+  useMaterial3: true,
+);
